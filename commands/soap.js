02661db8c9792abf096cb5652436dfe4714d8eb8
Cody Frick <frick_95@yahoo.com>
2020-02-07 20:09:29 -0500

    Added the "soap" command, that responds with a meme video.

---
+++ commands/soap.js
@@ -0,0 +1,4 @@
+exports.run = (client, message, args) => {
+    message.channel.send(':fire::athletic_shoe::athletic_shoe::fire: Soap or Die');
+    message.channel.send('https://www.youtube.com/watch?v=7N6x49ASwtU');
+}
